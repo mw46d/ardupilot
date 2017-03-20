d56ed01ed704daf4dd2f761cddbb556763e099ac
Marco Walther <marco@sonic.net>
2017-03-20 10:08:40 -0700

    GPS - Allow for a third GPS receiver - no MAVLink/log support:-(

--- libraries/AP_GPS/AP_GPS.cpp
+++ libraries/AP_GPS/AP_GPS.cpp
@@ -274,6 +274,59 @@ const AP_Param::GroupInfo AP_GPS::var_info[] = {
     AP_SUBGROUPINFO(params[1], "2_", 33, AP_GPS, AP_GPS::Params),
 #endif
 
+    // MARCO
+    // @Param: TYPE3
+    // @DisplayName: 3rd GPS type
+    // @Description: GPS type of 3rd GPS
+    // @Values: 0:None,1:AUTO,2:uBlox,3:MTK,4:MTK19,5:NMEA,6:SiRF,7:HIL,8:SwiftNav,9:PX4-UAVCAN,10:SBF,11:GSOF,12:QURT,13:ERB,14:MAV,15:NOVA
+    // @RebootRequired: True
+    // @User: Advanced
+    AP_GROUPINFO("TYPE3", 22, AP_GPS, _type[2], 0),
+
+    // @Param: GNSS_MODE3
+    // @DisplayName: GNSS system configuration
+    // @Description: Bitmask for what GNSS system to use on the third GPS (all unchecked or zero to leave GPS as configured)
+    // @Values: 0:Leave as currently configured, 1:GPS-NoSBAS, 3:GPS+SBAS, 4:Galileo-NoSBAS, 6:Galileo+SBAS, 8:Beidou, 51:GPS+IMES+QZSS+SBAS (Japan Only), 64:GLONASS, 66:GLONASS+SBAS, 67:GPS+GLONASS+SBAS
+    // @Bitmask: 0:GPS,1:SBAS,2:Galileo,3:Beidou,4:IMES,5:QZSS,6:GLOSNASS
+    // @User: Advanced
+    AP_GROUPINFO("GNSS_MODE3", 23, AP_GPS, _gnss_mode[2], 0),
+
+    // @Param: RATE_MS3
+    // @DisplayName: GPS 3 update rate in milliseconds
+    // @Description: Controls how often the GPS should provide a position update. Lowering below 5Hz is not allowed
+    // @Units: milliseconds
+    // @Values: 100:10Hz,125:8Hz,200:5Hz
+    // @User: Advanced
+    AP_GROUPINFO("RATE_MS3", 24, AP_GPS, _rate_ms[2], 200),
+
+    // @Param: POS3_X
+    // @DisplayName: Antenna X position offset
+    // @Description: X position of the third GPS antenna in body frame. Positive X is forward of the origin. Use antenna phase centroid location if provided by the manufacturer.
+    // @Units: m
+    // @User: Advanced
+
+    // @Param: POS3_Y
+    // @DisplayName: Antenna Y position offset
+    // @Description: Y position of the third GPS antenna in body frame. Positive Y is to the right of the origin. Use antenna phase centroid location if provided by the manufacturer.
+    // @Units: m
+    // @User: Advanced
+
+    // @Param: POS3_Z
+    // @DisplayName: Antenna Z position offset
+    // @Description: Z position of the third GPS antenna in body frame. Positive Z is down from the origin. Use antenna phase centroid location if provided by the manufacturer.
+    // @Units: m
+    // @User: Advanced
+    AP_GROUPINFO("POS3", 25, AP_GPS, _antenna_offset[2], 0.0f),
+
+    // @Param: DELAY_MS3
+    // @DisplayName: GPS 3 delay in milliseconds
+    // @Description: Controls the amount of GPS  measurement delay that the autopilot compensates for. Set to zero to use the default delay for the detected GPS type.
+    // @Units: milliseconds
+    // @Range: 0 250
+    // @User: Advanced
+    AP_GROUPINFO("DELAY_MS3", 26, AP_GPS, _delay_ms[2], 0),
+    // END MARCO
+
     AP_GROUPEND
 };
 
--- libraries/AP_GPS/AP_GPS.h
+++ libraries/AP_GPS/AP_GPS.h
@@ -31,6 +31,15 @@
 #include <GCS_MAVLink/GCS_MAVLink.h>
 
 #define GPS_UNKNOWN_DOP UINT16_MAX // set unknown DOP's to maximum value, which is also correct for MAVLink
+/**
+   maximum number of GPS instances available on this platform. If more
+   than 1 then redundant sensors may be available
+ */
+#define GPS_MAX_RECEIVERS 3 // maximum number of physical GPS sensors allowed - does not include virtual GPS created by blending receiver data
+#define GPS_MAX_INSTANCES  (GPS_MAX_RECEIVERS + 1) // maximumum number of GPs instances including the 'virtual' GPS created by blending receiver data
+#define GPS_BLENDED_INSTANCE GPS_MAX_RECEIVERS  // the virtual blended GPS is always the highest instance (2)
+#define GPS_RTK_INJECT_TO_ALL 127
+#define GPS_MAX_RATE_MS 200 // maximum value of rate_ms (i.e. slowest update rate) is 5hz or 200ms
 
 // the number of GPS leap seconds - copied into SIM_GPS.cpp
 #define GPS_LEAPSECONDS_MILLIS 18000ULL
