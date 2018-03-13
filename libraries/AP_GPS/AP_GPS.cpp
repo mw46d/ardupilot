a5d808f9bb1b41ddde15720ef77ac0388074b5f1
Marco Walther <marco@sonic.net>
2018-03-12 22:56:14 -0700

    I have more than two GPS receivers

--- libraries/AP_GPS/AP_GPS.cpp
+++ libraries/AP_GPS/AP_GPS.cpp
@@ -1541,7 +1541,28 @@ bool AP_GPS::all_consistent(float &distance) const
     }
 
     // calculate distance
-    distance = state[0].location.get_distance_NED(state[1].location).length();
+#ifdef ORIGINAL // MARCO
+    distance = location_3d_diff_NED(state[0].location, state[1].location).length();
+#else
+    Location l1 = state[0].location;
+    bool have_one = false;
+
+    for (int i = 0; i < num_instances; i++) {
+        if (state[i].status >= GPS_OK_FIX_2D) {
+            if (have_one) {
+                float d = location_3d_diff_NED(l1, state[i].location).length();
+
+                if (d > distance) {
+                    distance = d;
+                }
+            }
+
+            l1 = state[i].location;
+            have_one = true;
+        }
+    }
+#endif
+
     // success if distance is within 50m
     return (distance < 50);
 }
