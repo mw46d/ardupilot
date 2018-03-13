4dfdc4e76792452cf799a77ea196a7ce9461e7d8
Marco Walther <marco@sonic.net>
2018-03-12 22:58:50 -0700

    Use the hardware safety switch as arm switch - 2nd

--- libraries/AP_HAL/Util.h
+++ libraries/AP_HAL/Util.h
@@ -94,6 +94,7 @@ public:
       return state of safety switch, if applicable
      */
     virtual enum safety_state safety_switch_state(void) { return SAFETY_NONE; }
+    virtual void set_safety_switch(enum safety_state state) { return; } // MARCO
 
     /*
       set HW RTC in UTC microseconds
